51658af6983e43a661ee92220c4e1faf16f581a4
dylanvazquezzz <dylanv0909@gmail.com>
2026-05-12 17:00:26 -0400

    chore: remove accidentally committed Google Ads saved webpage files
    581 browser-saved asset files were committed to the images/ folder,
    containing Google's own embedded API keys in their minified JS.
    These are not our keys, but cleaning up to resolve GitHub secret scanning alerts.
    
    Updated .gitignore to block *_files/ and *.html patterns.

--- images/Search keywords - 961-544-5396 - Google Ads_files/main.dart.js_2453.part.js
+++
@@ -1,8 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B
-A=c[0]
-B=c[1265]
-var z=a.updateTypes([]);(function constants(){var y=a.makeConstList
-B.bZt=y(["id"],A.p("q<l>"))})()};
-(a=>{a["+yzybH9CLtPJxStjjZ5ttffJuBI="]=a.current})($__dart_deferred_initializers__);
-//# sourceMappingURL=main.dart.js_2453.part.js.map
